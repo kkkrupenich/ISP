8a38f7b76dd314dd451d0e15c8db73da6b8a5d8c
dream <80534443+kkkrupenich@users.noreply.github.com>
2021-04-08 20:36:57 +0300

    Add files via upload

---
+++ lab4/4.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MusicPlayer
+{
+    class Program
+    {
+        [DllImport("winmm.dll")]
+        static extern bool PlaySound(string sound, IntPtr hmod, int flags = 0x0001);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose which one u want to listen Bounce(1) or folva(2)?");
+            string music = Console.ReadLine();
+            while (true)
+            {
+                switch (music)
+                {
+                    case "1":
+                        Console.WriteLine("Playing 'IHAVEONECHANCE & Hies - Bounce'");
+                        PlaySound("../4.1/phonk1.wav", IntPtr.Zero);
+                        break;
+                    case "2":
+                        Console.WriteLine("Playing 'Eddie - folva'");
+                        PlaySound("../4.1/phonk2.wav", IntPtr.Zero);
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect input. Try again.");
+                        break;
+                }
+                Console.WriteLine("S - stop playing music and choose next one, E - exit");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "S":
+                        Console.WriteLine("Stop playing");
+                        PlaySound(" ", IntPtr.Zero);
+                        Console.WriteLine("Choose the next one:");
+                        music = Console.ReadLine();
+                        break;
+                    case "E":
+                        Console.WriteLine("Good bye!");
+                        return;
+                    default:
+                        Console.WriteLine("Incorrect input. Try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ lab4/4.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MyDLLImport
+{
+    class Program
+    {
+        [DllImport("DefaultMath.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Summ(int a, int b);
+
+        [DllImport("DefaultMath.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Minus(int a, int b);
+
+        [DllImport("DefaultMath.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Multi(int a, int b);
+
+        [DllImport("DefaultMath.dll", CallingConvention = CallingConvention.Winapi)]
+        public static extern int Div(int a, int b);
+
+
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Введите первое число");
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+
+            Console.WriteLine("Введите целое второе число");
+            while (!int.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }
+            Console.WriteLine("Сумма чисел: {0}", Summ(a, b));
+            Console.WriteLine("Разница чисел: {0}", Minus(a, b));
+            Console.WriteLine("Произведение чисел: {0}", Multi(a, b));
+            Console.WriteLine("Частное чисел: {0}", Div(a, b));
+            Console.ReadKey();
+        }
+    }
+}
