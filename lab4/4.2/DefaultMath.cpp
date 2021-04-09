900f021099dbc2b19d90cb087810d7da244eb004
dream <80534443+kkkrupenich@users.noreply.github.com>
2021-04-09 17:33:21 +0300

    Add files via upload

---
+++ lab4/4.2/DefaultMath.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+#include "DefaultMath.h"
+#include <iostream>
+
+int Summ(int a, int b)
+{
+	return a + b;
+}
+
+int Minus(int a, int b)
+{
+	return a - b;
+}
+
+int Multi(int a, int b)
+{
+	return a * b;
+}
+
+int Div(int a, int b)
+{
+	return a / b;
+}
---
+++ lab4/4.2/DefaultMath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+
+extern "C" __declspec(dllexport) int Summ(int a, int b);
+extern "C" __declspec(dllexport) int Minus(int a, int b);
+extern "C" __declspec(dllexport) int Multi(int a, int b);
+extern "C" __declspec(dllexport) int Div(int a, int b);
